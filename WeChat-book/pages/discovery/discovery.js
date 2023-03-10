946fa7dc4dd12d683330758593301c87b275b05b
liyiwen <liyiwen1013@foxmail.com>
2023-03-10 22:37:16 +0800

    添加小程序代码

---
+++ WeChat-book/pages/discovery/discovery.js
@@ -0,0 +1,23 @@
+// pages/discovery/discovery.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
