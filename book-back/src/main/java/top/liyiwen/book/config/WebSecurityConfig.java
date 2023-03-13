611a305b52a9c53b05a03c32cbbca63ed0c8b582
linzeliang <linzeliang1222@gmail.com>
2023-03-13 14:27:02 +0800

    完成基础框架搭建

--- book-back/src/main/java/top/liyiwen/book/config/WebSecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package top.liyiwen.book.config;/**
- * TODO
- *
- * @Author: linzeliang
- * @Date: 2023/3/11
- */
-public class WebSecurityConfig {
-}
--- book-back/src/main/java/top/liyiwen/book/utils/JwtUtil.java
+++
@@ -1,8 +0,0 @@
-package top.liyiwen.book.utils;/**
- * TODO
- *
- * @Author: linzeliang
- * @Date: 2023/3/11
- */
-public class JwtUtil {
-}
